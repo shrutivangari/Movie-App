132ed38993b06deb7648c2726a2d0e454fbc46e6
Shruti Vangari <shruti.vangari@cerner.com>
2015-10-30 08:52:49 -0500

    Adding migration, seed data

--- db/migrate/20151030134715_create_parts.rb
+++ db/migrate/20151030134715_create_parts.rb
@@ -1,7 +1,8 @@
 class CreateParts < ActiveRecord::Migration
   def change
     create_table :parts do |t|
-
+      t.belongs_to :movie, index: true
+      t.belongs_to :actor, index: true
       t.timestamps null: false
     end
   end
